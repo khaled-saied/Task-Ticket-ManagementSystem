75f184b009f18adae420aafad6178c08ff1cf0f1
khaled saied <khaledsaied080@gmail.com>
2025-05-20 09:12:32 +0300

    2=> Add All Dtos Of Project

---
+++ BLL/DataTransferObjects/ProjectDtos/ProjectDetailsDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DataTransferObjects.ProjectDtos
+{
+    public class ProjectDetailsDto
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public string OwnerName { get; set; } = string.Empty;
+
+        //public List<TaskDto> Tasks { get; set; } = new();
+
+    }
+}

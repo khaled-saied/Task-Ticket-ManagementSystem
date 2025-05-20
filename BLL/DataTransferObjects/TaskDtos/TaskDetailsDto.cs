47fdfb927c7b82ba308fecb34decc258e7e24868
khaled saied <khaledsaied080@gmail.com>
2025-05-20 09:24:20 +0300

    3=> Add Dtos Of Task

---
+++ BLL/DataTransferObjects/TaskDtos/TaskDetailsDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.DataTransferObjects.ProjectDtos;
+
+namespace BLL.DataTransferObjects
+{
+    public class TaskDetailsDto
+    {
+        //Id, Title, Description, Status, DueDate, ProjectDto, List<TicketDto>
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public DateTime DueDate { get; set; }
+        public ProjectDto Project { get; set; }
+        //public List<TicketDto> Tickets { get; set; }
+    }
+}
---
+++ BLL/DataTransferObjects/TaskDtos/UpdateTaskDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DataTransferObjects.TaskDtos
+{
+    public class UpdateTaskDto
+    {
+        //Id, Title, Description, Status, DueDate
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public DateTime DueDate { get; set; }
+
+    }
+}

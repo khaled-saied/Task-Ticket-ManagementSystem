6eed07f0e5537a15dc2dc7bfdb719240d6a72720
khaled saied <khaledsaied080@gmail.com>
2025-05-20 10:20:18 +0300

    6=> Add Mapping For All Models

---
+++ BLL/Profiles/CommentProfile.cs
@@ -0,0 +1,24 @@
+﻿
+namespace BLL.Profiles
+{
+    public class CommentProfile : Profile
+    {
+        public CommentProfile()
+        {
+            // CreateCommentDto → Comment
+            CreateMap<CreateCommentDto, Comment>();
+
+            // UpdateCommentDto ←→ Comment
+            CreateMap<UpdateCommentDto, Comment>().ReverseMap();
+
+            // Comment → CommentDto (عرض بسيط)
+            CreateMap<Comment, CommentDto>();
+
+            // Comment → CommentDetailsDto
+            CreateMap<Comment, CommentDetailsDto>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.User.FullName)) // لو عندك FullName في ApplicationUser
+                .ForMember(dest => dest.TicketDto, opt => opt.MapFrom(src => src.Ticket)); // لازم TicketDto يكون مضاف مسبقاً في AutoMapper
+
+        }
+    }
+}

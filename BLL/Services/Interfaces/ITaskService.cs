e80b2640adaba1f7b168ca0f7485967611344ea6
khaled saied <khaledsaied080@gmail.com>
2025-05-22 00:21:25 +0300

    2=> TaskService

---
+++ BLL/Services/Interfaces/ITaskService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services.Interfaces
+{
+    public interface ITaskService
+    {
+        Task<IEnumerable<TaskDto>> GetAllTasks();
+        Task<TaskDetailsDto> GetTaskById(int id);
+        Task<int> CreateTask(CreateTaskDto createTaskDto);
+        Task<int> UpdateTask(UpdateTaskDto updateTaskDto);
+        Task<bool> DeleteTask(int id);
+    }
+}

affa2656ea803e1465a361f767d454a8f0da5f29
khaled saied <khaledsaied080@gmail.com>
2025-05-28 00:56:45 +0300

    1=> Add Index Action Of Ticket

---
+++ Ticket ManagementSystem/Controllers/TicketController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using BLL.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ticket_ManagementSystem.Controllers
+{
+    public class TicketController(IServiceManger _serviceManger,
+                                  ILogger<TicketController> _logger,
+                                  IWebHostEnvironment _environment) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var tickets = await _serviceManger.TicketService.GetAllAsync();
+            return View(tickets);
+        }
+    }
+}
